8b07072dcc0a07860a555bd1e01fdf0fcd252bc1
Jack Miller <contact@jackmiller.design>
2021-04-06 08:16:38 +0100

    [initial dev] contact form and page sorted

--- wp-content/themes/assemblage/page-contact.php
+++ wp-content/themes/assemblage/page-contact.php
@@ -5,7 +5,7 @@
 get_header(); ?>
 
 <div class="[ l-2col ]">
-	<div class="[ l-2col__col l-2col__col--space-between ]">
+	<div class="[ l-2col__col l-2col__col--space-between ][ contact-content-wrap ]">
 		<div class="[ contact-header ]">
 			<h1><?= (get_field('page_title')) ? get_field('page_title') : get_the_title(); ?></h1>
 		</div>
@@ -14,7 +14,10 @@ get_header(); ?>
 		</div>
 
 	</div>
-	<div class="[ l-2col__col ]">
+	<div class="[ l-2col__col ][ contact-image-wrap ]">
+		<div class="[ contact-header-mobile ]">
+			<h1><?= (get_field('page_title')) ? get_field('page_title') : get_the_title(); ?></h1>
+		</div>
 		<?= LazyImage::get_image(get_post_thumbnail_id(), [50, 100], 'contact-image', true, false); ?>
 	</div>
 </div>
--- wp-content/themes/assemblage/src/js/Form.js
+++ wp-content/themes/assemblage/src/js/Form.js
@@ -14,16 +14,77 @@ class Form extends Component {
 
 	mount() {
 
+		// setting up first empty option with the needed data attributes from slim select
+		const selectTag = this.element.querySelector('select')
+		const firstOption = selectTag.querySelector('option:nth-child(1)')
+		firstOption.setAttribute('data-placeholder', 'true')
+		firstOption.removeAttribute('value')
+
 		const select = new SlimSelect({
 			select: '.wpcf7-select',
 			showSearch: false,
 			showContent: 'down',
 			hideSelectedOption: true,
+			placeholder: 'Nature of Enquirey*',
 		})
 
-		// setting first option as placeholder
-		select.data.data[0].placeholder = true
+		this.placeholders()
+
+		document.addEventListener( 'wpcf7mailsent', (event) => {
+			this.resetAllLables()
+		}, false );
 	}
+
+	placeholders() {
+		const inputs = this.element.querySelectorAll('input[type="text"], input[type="email"], input[type="tel"], textarea')
+		const inputsArray = Array.from(inputs)
+
+		inputsArray.forEach((input) => {
+
+			const parentElement = input.parentElement.parentElement
+			const label = parentElement.querySelector('label')
+
+			input.addEventListener('keyup', () => {
+				this.activateLabel(label, input)
+			})
+
+			// acticate when input is focused
+			input.addEventListener('focus', () => {
+				this.activateLabel(label, input)
+			})
+
+			// when input is not the active element
+			input.addEventListener('blur', () => {
+				this.activateLabel(label, input)
+			})
+		})
+	}
+
+	activateLabel(label, input) {
+
+		if(input.value.length != 0 || input === document.activeElement) {
+			label.classList.add('js-fake-placeholder')
+		} else {
+			label.classList.remove('js-fake-placeholder')
+		}
+	}
+
+	resetAllLables() {
+
+		const inputs = this.element.querySelectorAll('input[type="text"], input[type="email"], input[type="tel"], textarea')
+		const inputsArray = Array.from(inputs)
+
+		inputsArray.forEach((input) => {
+
+			const parentElement = input.parentElement.parentElement
+			const label = parentElement.querySelector('label')
+
+			if(label.classList.contains('js-fake-placeholder')) {
+				label.classList.remove('js-fake-placeholder')
+			}
+		})
+	}
+
 }
 
 // Export ES6 module
