935e0f52c1d8b7fd1cc5895c3eb43438f67b97de
Jack Miller <contact@jackmiller.design>
2021-04-11 17:41:11 +0100

    [initial dev] privacy page class missing

--- wp-content/themes/assemblage/page-privacy-policy.php
+++ wp-content/themes/assemblage/page-privacy-policy.php
@@ -5,12 +5,14 @@
 get_header(); ?>
 
 <div class="[ l-2col l-2col--top ]">
-	<div class="[ l-2col__col l-2col__col--space-between ]">
-		<h1 class="[ l-2col__sticky l-2col__header l-2col__header--large ]"><?= (get_field('page_title')) ? get_field('page_title') : get_the_title(); ?></h1>
-	</div>
-	<div class="[ l-2col__col ]">
-		<div class="[ privacy-content ]">
-			<?= get_the_content(); ?>
+	<div class="[ l-2col__inner ]">
+		<div class="[ l-2col__col l-2col__col--space-between ]">
+			<h1 class="[ l-2col__sticky l-2col__header l-2col__header--large ]"><?= (get_field('page_title')) ? get_field('page_title') : get_the_title(); ?></h1>
+		</div>
+		<div class="[ l-2col__col ]">
+			<div class="[ privacy-content ]">
+				<?= get_the_content(); ?>
+			</div>
 		</div>
 	</div>
 </div>
